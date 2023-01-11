dcab180e70f5385866a60b799c0e58c01a8b2932
hyeg0121 <hyeg0121@naver.com>
2023-01-11 16:33:22 +0900

    article 원형 배치

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const frame = document.querySelector("section");
+const lists = frame.querySelectorAll("article");
+const deg = 45; //각각의 article 요소가 회전할 각도
+const len = lists.length-1 //순번이 0부터 시작하므로 전체 개수에서 1을 뺌
+let i = 0;
+
+for(let el of lists){
+    el.style.transform = 'rotate(${deg*i}deg) translateY(-100vh)';
+    i++;
+}
