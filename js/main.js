b55a7d5317828bd790ad34c17f2c15cf1d6e97f4
hyeg0121 <hyeg0121@naver.com>
2023-03-12 16:09:35 +0900

    버튼을 누르면 음반이 패널이 넘어감

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ const frame = document.querySelector("section");
 const lists = frame.querySelectorAll("article");
 const deg = 45; //각각의 article 요소가 회전할 각도
 const len = lists.length-1 //순번이 0부터 시작하므로 전체 개수에서 1을 뺌
+
 let i = 0;
+
+let active = 0;
 
 for(let el of lists){
     let pic = el.querySelector(".pic");
@@ -10,3 +13,35 @@ for(let el of lists){
     pic.style.backgroundImage = `url(./images/img${i+1}.jpg)`;
     i++;
 }
+
+//이동 버튼
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
+
+let num = 0;
+
+prev.addEventListener("click", ()=>{
+    num++;
+    frame.style.transform = `rotate(${deg * num}deg)`;
+
+    // 현재 패널의 순번이 0
+    (active == 0) ? active = len : active--;
+    activation(active, lists);
+});
+
+next.addEventListener("click", ()=>{
+    num--;
+    frame.style.transform = `rotate(${deg * num}deg)`;
+
+    // 현재 패널의 순번이 0
+    (active == 0) ? active = 0 : active++;
+    activation(active, lists);
+});
+
+function activation(index, lists){
+    for (let el of lists){
+        el.classList.remove("on");
+    }
+    lists[index].classList.add("on");
+}
+
